d01ccc13e31ff4811af5d9d2b26ff881e9de6052
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2025-03-26 18:31:18 +0000

    added support for redoc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
+import redoc from'redoc-express'
 import fs from 'fs'
 import * as yaml from 'yaml'
 import cors from 'cors'
@@ -16,7 +17,7 @@ const isSupported = (fileName: String) =>
 app.get('/', function (req: any, res: any) {
     const files = fs.readdirSync("resources").reduce((acc: string, fileName: string) =>
             isSupported(fileName) ?
-                `${acc}<li>${fileName} <a href="raw/${fileName}">raw</a> <a href="api-doc/${fileName}">swagger</a></li>
+                `${acc}<li>${fileName} <a href="raw/${fileName}">raw</a> <a href="swagger-ui/${fileName}">swagger ui</a> <a href="redoc/${fileName}">redoc</a></li>
 `
                 : acc,
         ''
@@ -49,7 +50,7 @@ app.use('/raw/:file', cors(), swaggerUi.serve, (req: any, res: any) => {
     res.send(file)
 })
 
-app.use('/api-doc/:file', swaggerUi.serve, (req: any, res: any, err: any) => {
+app.use('/swagger-ui/:file', swaggerUi.serve, (req: any, res: any, err: any) => {
     const fileName = req.params.file
     const file = fs.readFileSync(`resources/${fileName}`, 'utf8')
     const swaggerDocument =
@@ -58,6 +59,41 @@ app.use('/api-doc/:file', swaggerUi.serve, (req: any, res: any, err: any) => {
             : file
     swaggerUi.setup(swaggerDocument)(req, res, err)
 })
+
+// serve your swagger.json file
+app.get('/redoc/:file',  (req :any, res:any) => {
+    const fileName = req.params.file
+    redoc({
+        title: 'API Docs',
+        specUrl: `../raw/${fileName}`,
+        nonce: '', // <= it is optional,we can omit this key and value
+                   // we are now start supporting the redocOptions object
+                   // you can omit the options object if you don't need it
+                   // https://redocly.com/docs/api-reference-docs/configuration/functionality/
+        redocOptions: {
+        theme: {
+            colors: {
+                primary: {
+                    main: '#6EC5AB'
+                }
+            },
+            typography: {
+                fontFamily: `"museo-sans", 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+                    fontSize: '15px',
+                    lineHeight: '1.5',
+                    code: {
+                    code: '#87E8C7',
+                        backgroundColor: '#4D4D4E'
+                }
+            },
+            menu: {
+                backgroundColor: '#ffffff'
+            }
+        }
+        }
+    })(req, res)
+})
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
